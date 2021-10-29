66c4dbfe021f4d0cfe9d2593410909c0c9ffa3a7
Franco <Zalamena>
2021-10-29 12:50:20 +0100

    Concurrencies Corrected

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
-const PORT = process.env.PORT || 3000;;
+const PORT = process.env.PORT || 8000;;
 
 const axios = require('axios');
-const { response } = require('express');
 
+const queue = require('queue')
+
+
+
+// var chessAPI = new ChessWebAPI({
+//     queue: true,
+// });
+
+// let printResults = function(response, error, sampleParameter1, sampleParameter2){
+//     console.log(response.body);
+//     console.log(sampleParameter1);
+//     console.log(sampleParameter2);
+// }
+// chessAPI.dispatch(chessAPI.getPlayerMonthlyArchives, printResults, ["francozalamena"])
 
 function convertTimeIntoSeconds(string){
     var split = string.split(":")
@@ -15,61 +28,91 @@ function convertTimeIntoSeconds(string){
     return seconds + minutes * 60 + hours * 3600
 }
 
-function getTotalTimePlayed(urlsRes, res){
-    
-    var promises = []
-    for(url in urlsRes.archives){
-        var promise = axios.get(urlsRes.archives[url])
-        promises.push(promise)
-    }
-    return Promise.all(promises)
-}
+async function getTotalTimeFromArchive(url){
+    let gamesResponse = await axios.get(url)
+    let games = gamesResponse.data.games
+    let totalTimePlayed = 0
+    return new Promise(function(resolve, reject){
+        for(gameIndex in games){
+            let game = games[gameIndex]
+            var pgn = game.pgn
+            if(pgn != undefined){
+                var formattedString = pgn.replace(/\[/g, "").replace(/\]/g, "").replace(/\n/g, "\n").replace(/\\/g, "")
+                var date =  formattedString.match(/^Date (["0-9.]*)$/gm)[0].split(" ")[1].replace("\"", "")
+                var startTime =  formattedString.match(/^StartTime (["0-9:]*)$/gm)[0].split(" ")[1].replace("\"", "")
+                var endTime =  formattedString.match(/^EndTime (["0-9:]*)$/gm)[0].split(" ")[1].replace("\"", "")
 
-app.get('/chesscom/:userId', (req, res) => {
-    var userId = req.params.userId
-    axios.get("https://api.chess.com/pub/player/" + userId + "/games/archives")
-        .then(async urlsRes => {
-            var promisesCompleted = await getTotalTimePlayed(urlsRes.data, res)
-            var totalTimePlayed = 0
-            for(i in promisesCompleted){
-                var gamesRes = promisesCompleted[i]
-                for(game in gamesRes.data.games){
-                    var gameJson = gamesRes.data.games[game]
-                    var pgn = gameJson.pgn
-                    if(pgn != undefined){
-                        var formattedString = pgn.replace(/\[/g, "").replace(/\]/g, "").replace(/\n/g, "\n").replace(/\\/g, "")
-                        var date =  formattedString.match(/^Date (["0-9.]*)$/gm)[0].split(" ")[1].replace("\"", "")
-                        var startTime =  formattedString.match(/^StartTime (["0-9:]*)$/gm)[0].split(" ")[1].replace("\"", "")
-                        var endTime =  formattedString.match(/^EndTime (["0-9:]*)$/gm)[0].split(" ")[1].replace("\"", "")
-        
-                        var startSeconds = convertTimeIntoSeconds(startTime)
-                        var endSeconds = convertTimeIntoSeconds(endTime)
-        
-                        var totalTime = endSeconds - startSeconds
-        
-                        if (totalTime > 0){
-                            // console.log(date)
-                            // console.log(totalTime)
-                            // console.log("\n")
-                            totalTimePlayed = totalTimePlayed + totalTime
-                        }
-                    }else{
-                        // console.log(gameJson)
-                        continue
-                    }
+                var startSeconds = convertTimeIntoSeconds(startTime)
+                var endSeconds = convertTimeIntoSeconds(endTime)
+
+                var totalTime = endSeconds - startSeconds
+
+                if (totalTime > 0){
+                    // console.log(date)
+                    // console.log(totalTime)
+                    // console.log("\n")
+                    totalTimePlayed = totalTimePlayed + totalTime
                 }
             }
-            res.status(200).send(String(totalTimePlayed))
-        })
-        .catch(error => {
-            res.send(error)
         }
-        )
-});
+        
+        resolve(totalTimePlayed)
+    }).catch(function(err){
+        console.log(err)
+    })
+}
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000/'
+}));
+
+app.get('/chesscom/:userId',cors(), async (req, res) => {
+    var userId = req.params.userId
+    console.log('request from ' + req.headers.origin)
+    const urlsRes = await axios.get("https://api.chess.com/pub/player/" + userId + "/games/archives").catch(function(error){
+        res.status(error.response.status).send(error.response.statusText)
+        return
+    })
+    if(urlsRes == undefined) return
+    
+    const q = queue({results: [], concurrency : 2})
+
+    var totalTimePlayed =  0
+    for(urlIndex in urlsRes.data.archives){ 
+        let url = urlsRes.data.archives[urlIndex]
+        //instead of actually calling it, we need to add it into the queue
+        q.push(() => getTotalTimeFromArchive(url))
+    }
+
+    q.start(function(err){
+        if(err){
+            console.log(err.response.statusText)
+            res.status(err.response.status).send(err.response.statusText)
+            q.end([err])
+        } else{
+            for(i in q.results){
+                totalTimePlayed += q.results[i][0]
+            }
+    
+            var json = {
+                totalTime: String(totalTimePlayed)
+            }
+            res.status(200).json(json)
+            console.log('sending info to client: ' + JSON.stringify(json))
+            q.end()
+        }   
+    })
+
+    q.on('success', function (result, job) {
+        console.log('job finished processing:', job.toString().replace(/\n/g, ''))
+      })
+
+    
+
+
+    
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`)
 });
-
